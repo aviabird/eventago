06dc8709a7f698f86a31eddc1e63eae9da29d7f6
Harshal pawar <harshal123pawar@gmail.com>
2019-11-13 15:58:15 +0530

    skeleton for CQRS library.

--- cqrs.go
+++ cqrs.go
@@ -1,8 +1,3 @@
-// Copyright 2016 Jet Basrawi. All rights reserved.
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file.
-
 // Package cqrs provides a CQRS reference implementation.
 //
 package cqrs
@@ -12,10 +7,6 @@ import (
 	"reflect"
 )
 
-// typeOf is a convenience function that returns the name of a type
-//
-// This is used so commonly throughout the code that it is better to
-// have this convenience function and also allows for changing the scheme
 // used for the type name more easily if desired.
 func typeOf(i interface{}) string {
 	return reflect.TypeOf(i).Elem().Name()
@@ -27,11 +18,6 @@ func NewUUID() string {
 }
 
 // Int returns a pointer to int.
-//
-// There are a number of places where a pointer to int
-// is required such as expectedVersion argument on the repository
-// and this helper function makes keeps the code cleaner in these
-// cases.
 func Int(i int) *int {
 	return &i
 }
